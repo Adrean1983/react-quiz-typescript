2519a0c6adb244654b8b1b6028d852976afdd02b
Adrean1983 <adrean_@hotmail.com>
2020-10-25 15:06:12 -0400

    exercise starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 const App = () => {
-  return <div className="App">Quiz</div>;
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => [];
+
+  return (
+    <div className="App">
+      <h1>REACT QUIZ</h1>
+      <button className="start">Start</button>
+      <p className="score">Score:</p>
+      <p>Loading Questions ...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
 };
 
 export default App;
