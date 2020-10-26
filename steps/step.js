a85c70d984cad7fe816d7f0bc8b6737f895d0541
Adrean1983 <adrean_@hotmail.com>
2020-10-25 20:13:03 -0400

    end of exercise 1

---
+++ steps/step.js
@@ -0,0 +1,10 @@
+const step1 = {
+  //   Add types for the props in QuestionCard make QuestionCard a type. And expost props to component as normal.
+  // question
+  // answers
+  // callback
+  // userAnswer
+  // questionNr
+  // totalQuestions
+  // Display the question number and total questions in a p tag with class of number. Display the question in a p tag using dangerously set innerhtml. Map over the answers in a div with a button with disabled based on the userAnswer. It also has an onClick that calls callback. Within button show the answer in a span using dangerously set inner HTML.
+};
