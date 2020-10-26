2fdae486874b20a3512fe3ae1cb3e5d85029ee34
Adrean1983 <adrean_@hotmail.com>
2020-10-25 20:24:04 -0400

    step 2 exercise setup

--- steps/solution.js
+++ steps/solution.js
@@ -1,10 +1,10 @@
 const step1 = {
   //   import React from 'react';
   // type Props = {
-  //   question: string;
+  //   questions: string;
   //   answers: string[];
   //   callback: any;
-  //   userAnswer: Boolean;
+  //   userAnswer: any;
   //   questionNr: number;
   //   totalQuestions: number;
   // };
@@ -28,4 +28,39 @@ const step1 = {
   // export default QuestionCard;
 };
 
-const step2 = {};
+const step2 = {
+  //   import React, { useState } from 'react';
+  // import QuestionCard from './components/QuestionCard';
+  // const TOTAL_QUESTIONS = 10;
+  // const App = () => {
+  //   const [loading, setLoading] = useState(false);
+  //   const [questions, setQuestions] = useState([]);
+  //   const [number, setNumber] = useState(0);
+  //   const [userAnswers, setUserAnswers] = useState([]);
+  //   const [score, setScore] = useState(0);
+  //   const [gameOver, setGameOver] = useState(true);
+  //   const startTrivia = async () => {};
+  //   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  //   const nextQuestion = () => [];
+  //   return (
+  //     <div className="App">
+  //       <h1>REACT QUIZ</h1>
+  //       <button className="start">Start</button>
+  //       <p className="score">Score:</p>
+  //       <p>Loading Questions ...</p>
+  //       <QuestionCard
+  //         questionNr={number + 1}
+  //         totalQuestions={TOTAL_QUESTIONS}
+  //         question={questions[number].question}
+  //         answers={questions[number].answer}
+  //         userAnswer={userAnswers ? userAnswers[number] : undefined}
+  //         callback={checkAnswer}
+  //       />
+  //       <button className="next" onClick={nextQuestion}>
+  //         Next Question
+  //       </button>
+  //     </div>
+  //   );
+  // };
+  // export default App;
+};
--- steps/step.js
+++ steps/step.js
@@ -3,8 +3,12 @@ const step1 = {
   // question
   // answers
   // callback
-  // userAnswer
+  // userAnswers
   // questionNr
   // totalQuestions
   // Display the question number and total questions in a p tag with class of number. Display the question in a p tag using dangerously set innerhtml. Map over the answers in a div with a button with disabled based on the userAnswer. It also has an onClick that calls callback. Within button show the answer in a span using dangerously set inner HTML.
+};
+
+const step2 = {
+  // Build props from the app for question card in app.js. Set state for loading, gameOver and the props passed down.
 };
